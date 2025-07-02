35ec2c8ae6ed41b2f06ad8335b8886680ddfc65e
lightspot-coder <214618438+lightspot-coder@users.noreply.github.com>
2025-07-02 17:59:10 +0200

    adding some rules in the new size of the grid

--- myscripts.js
+++ myscripts.js
@@ -1,24 +1,16 @@
 const divsContainer = document.querySelector("#container");
 let sizeGrid = 16;
 
-function hoverEffect(div){
-
-    div.setAttribute("style","background: blue");
-
-}
 
 function removeAllChild(size){
 
-    console.log("dentro de removeAllChild");
-    console.log(size);
     for(let i = 0; i < size ; i++){
        
         for(let j = 0; j < size ; j++){
             divsContainer.querySelector(".divRow").removeChild(divsContainer.querySelector(".subDiv"));
         }
         divsContainer.removeChild(divsContainer.querySelector(".divRow"));
-        
-        
+          
     }
     sizeGrid = 0;
 
@@ -29,11 +21,17 @@ newGridButton.textContent = "New Grid";
 newGridButton.addEventListener("click",() => {
             
     let newSizeGrid = +prompt("Enter the new size of the grid (max 100)","");
-    console.log(newSizeGrid);
-    removeAllChild(sizeGrid);
-    createGrid(newSizeGrid);
     
-    
+    if(newSizeGrid > 100){
+        alert("Too Big! max 100!");
+    }
+    else if(isNaN(newSizeGrid)){
+        alert("only numbers!");
+    }
+    else{
+        removeAllChild(sizeGrid);
+        createGrid(newSizeGrid);
+    } 
     
 });
 
@@ -43,7 +41,6 @@ function createGrid(size){
 
     sizeGrid = size;
     for(let i = 0; i < sizeGrid; i++){
-
 
         const divContainerRow = document.createElement("div");
         divContainerRow.classList.add("divRow");
